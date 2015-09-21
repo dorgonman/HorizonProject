9fbb457dc902b52a0d31ef8df3da2784d8d6c51c
dorgon <dorgonman@hotmail.com>
2015-09-22 01:11:20 +0800

    adjust script name

--- shell/win32/setup.sh
+++ shell/win32/setup.sh
@@ -5,15 +5,17 @@ pushd ../../HorizonScript
 CMD "/C setx HORIZON_SCRIPT_ROOT %cd%"
 popd
 echo "==============END register HORIZON_SCRIPT_ROOT path======================"
-echo "===============START build and Register BOOST_ROOT path====================="
+echo "===============START build and Register HORIZON_BOOST_ROOT path====================="
+
+#check boost folder exist, if not, download from official site
 
 pushd ../../boost_1_59_0
 
 CMD "/C bootstrap.bat"
 b2.exe
-CMD "/C setx BOOST_ROOT %cd%" 
+CMD "/C setx HORIZON_BOOST_ROOT %cd%" 
 popd
-echo "===============END build and Register BOOST_ROOT path====================="
+echo "===============END build and Register HORIZON_BOOST_ROOT path====================="
 
 
 #echo "===============START generate win32 project for HorizonCore====================="
